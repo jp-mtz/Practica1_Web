61fe0d689e1e6e8c18f9a0fab47e00543f95736d
jp-mtz <a01707046@tec.mx>
2023-11-11 22:39:32 +0100

    Nothing in particuar

--- Practica 2/src/enlaceRouter.js
+++ Practica 2/src/enlaceRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
 
-    res.render('index', {
+    res.render('RockolaMainPage', {
         posts: enlaceServicio.getPosts()
     });
 });
