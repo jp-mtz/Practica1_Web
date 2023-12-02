46a053b310fa198253c6e4cf65e8301181e0afc2
jp-mtz <a01707046@tec.mx>
2023-12-02 20:33:55 +0100

    Validate Edit page

--- Practica 2/src/enlaceRouter.js
+++ Practica 2/src/enlaceRouter.js
@@ -13,6 +13,41 @@ router.post('/post/new', (req,res) => {
     let{ artist, year, album, cover, genre, score, review, comment} = req.body;
     let error = "";
     if(artist === ""){
+        error = error + "Artist, ";}
+    if(year === ""){
+        error = error + "Year, ";}
+    if(album === ""){
+        error = error + "Album, ";}
+    if(cover === ""){
+        error = error + "Cover, ";}
+    if(genre === ""){
+        error = error + "Genre, ";}
+    if(score === ""){
+        error = error + "Score, ";}
+    if(review === ""){
+        error = error + "Review ";}
+    if (error.length > 0){
+        res.render('error_post', {error});}
+    else {
+        enlaceServicio.addPost({artist, year, album, cover, genre, score, review, comment});
+        res.render('saved_post');}
+});
+
+router.get('/post/:id', (req, res) => {
+    let post = enlaceServicio.getPost(req.params.id); 
+    res.render('RockolaPagDetallada', { post });
+});
+
+router.get('/post/:id/edit', (req, res) => {
+    let post = enlaceServicio.getPost(req.params.id); 
+    res.render('pagEditRockola', { post });
+});
+
+router.post('/post/:id/edit/edits', (req, res) => {
+    let id = req.params.id;
+    let{ artist, year, album, cover, genre, score, review, comment} = req.body;
+    let error = "";
+    if(artist === ""){
         error = error + "Artist, ";
     }
     if(year === ""){
@@ -28,42 +63,21 @@ router.post('/post/new', (req,res) => {
         error = error + "Genre, ";
     }
     if(score === ""){
-        error = error + "Score, ";
-    }
+        error = error + "Score, ";}
     if(review === ""){
-        error = error + "Review ";
-    }
-
+        error = error + "Review ";}
     if (error.length > 0){
-        res.render('error_post', {error});
-    }
+        res.render('error_post', {error});}
     else {
-        enlaceServicio.addPost({artist, year, album, cover, genre, score, review, comment});
+        enlaceServicio.editPost({artist, year, album, cover, genre, score, review, comment}, id);
         res.render('saved_post');
     }
-
-});
-
-router.get('/post/:id', (req, res) => {
-    let post = enlaceServicio.getPost(req.params.id); 
-    res.render('RockolaPagDetallada', { post });
-});
-
-router.get('/post/:id/edit', (req, res) => {
-    let post = enlaceServicio.getPost(req.params.id); 
-    res.render('pagEditRockola', { post });
-});
-
-router.post('/post/:id/edit/edits', (req, res) => {
-    let{ artist, year, album, cover, genre, score, review} = req.body;
-    let id = req.params.id;
-    enlaceServicio.editPost({artist, year, album, cover, genre, score, review}, id);
-    res.render('saved_post');
 });
 
 router.post('/post/:id/update', (req, res) => {
     let {artist, year, album, cover, genre, score, review, comment} = req.body;
-    let postId = req.params.id; 
+    let postId = req.params.id;
+    
     enlaceServicio.updatePost(postId, {artist, year, album, cover, genre, score, review, comment: { content: comment.content, user: comment.user, score1: comment.score1 } });
     res.redirect('/');
 });
